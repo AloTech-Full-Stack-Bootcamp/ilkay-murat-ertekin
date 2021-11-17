69f48206e88861f7b871b4ab0a1ba98561490cf8
ilkay murat ertekin <iertekinx@gmail.com>
2021-11-17 18:47:35 +0300

    Add perm for post route

--- week_3/smartEdu/routes/courseRoute.js
+++ week_3/smartEdu/routes/courseRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
-const permission = require('../middleware/permission')
+const {permRole,isAuthenticated} = require('../middleware/permission')
 
 // Course Routes
 router.route("/").get(courseController.getAllCourses);
-router.route("/").post(permission.isAuthenticated,courseController.createCourse);
+router.route("/").post(isAuthenticated,permRole(["teacher","admin"]),courseController.createCourse);
 router.route("/:slug").get(courseController.getCourseBySlug);
 router.route("/:slug").put(courseController.putCourse);
 router.route("/:slug").delete(courseController.deleteCourse);
