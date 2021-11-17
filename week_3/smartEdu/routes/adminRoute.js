1ea103784d5bbd975319cee4f92a2557fd503b40
ilkay murat ertekin <iertekinx@gmail.com>
2021-11-17 16:29:57 +0300

    Add Permission middleware

--- week_3/smartEdu/routes/adminRoute.js
+++ week_3/smartEdu/routes/adminRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const adminController =require('../controllers/adminController')
-const {isAuthenticated,isAdmin} = require('../middleware/permission')
+const {isAuthenticated,permRole} = require('../middleware/permission')
 
-router.route("/users").get(isAuthenticated,isAdmin,adminController.getAlluser);
+// Permission middleware
+router.use('/',isAuthenticated,permRole(['admin'])) 
+// Admin Routing
+router.route("/users").get(adminController.getAlluser);
 router.route("/:id").delete(adminController.deleteUser);
 router.route("/:id").get(adminController.getUser);
 
