5c693fbfa5bfde8fa3c62d8ed646e65efec35540
ilkay murat ertekin <iertekinx@gmail.com>
2021-11-17 16:27:36 +0300

    Edit App.js according to Config

--- week_3/smartEdu/app.js
+++ week_3/smartEdu/app.js
@@ -1,52 +1,22 @@
 const express = require("express")
-const mongoose = require("mongoose")
 const dotenv = require('dotenv')
-const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const cors =require('cors')
+const cors = require('cors')
+
+const Config = require('./config')
 
 const courseRoute = require("./routes/courseRoute")
 const userRoute = require('./routes/authRoute')
-const adminRoute=require('./routes/adminRoute')
+const adminRoute = require('./routes/adminRoute')
 
 app = express()
 dotenv.config()
-
-
-mongoose.connect(process.env.mongoUrl
-    , {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(() => {
-    console.log('DB CONNECTED!')
-}).catch((err) => {
-    console.log(err)
-})
-
-// Middleware
-app.use(cors({
-    'origin':'*',
-    'allowedHeaders':['sessionId', 'Content-Type']
-}))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(session(
-    {
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: process.env.mongoUrl }),
-    cookie:{
-        expires:new Date(Date.now()+(1200 * 1000))
-    }
-
-}))
+Config()
 
 //Routing
 app.use('/courses', courseRoute)
 app.use('/auth', userRoute)
-app.use('/admin',adminRoute)
+app.use('/admin', adminRoute)
 
 
 app.listen(process.env.PORT, () => {
