bdcfc1037563d5c3f095e549c3c5a827caab76e2
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-18 18:06:23 +0300

    Fix admin site

--- week_8/patikagram/main/admin.py
+++ week_8/patikagram/main/admin.py
@@ -11,15 +11,24 @@ class CommentAdmin(admin.TabularInline):
     readonly_fields = ['id', 'user']
 
 
+class LikeAdmin(admin.TabularInline):
+    model = Like
+    extra = 1
+    readonly_fields = ['id', 'user']
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'content', 'author',
-                    'created_at', '_likes_count', '_comments_count']
+                    'created_at', '_likes_count', '_comments_count', 'like_comment_count']
     list_filter = ['created_at']
     search_fields = ['author__username']
     autocomplete_fields = ['author']
     ordering = ['created_at']
-    inlines = [CommentAdmin]
+    inlines = [LikeAdmin, CommentAdmin]
+
+    def like_comment_count(self, post):
+        return f"Likes: {post._likes_count} / Comments: {post._comments_count}"
 
 
 @admin.register(Like)
