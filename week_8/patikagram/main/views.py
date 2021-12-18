42eba29e1c839462b46239822b4b5b56cc023368
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-18 18:10:38 +0300

    Fix multiple likes

--- week_8/patikagram/main/views.py
+++ week_8/patikagram/main/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .serializers import UserSerializer, PostSerializer, LikeSerializer, CommentSerializer
 from .models import Post, Like, Comment
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,6 +19,16 @@ class PostsViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def Like(self, request, pk=None):
+        post = self.get_object()
+        qs = Like.objects.get_or_create(post=post, user=request.user)
+        serializer = LikeSerializer(qs[0])
+        return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class LikeViewSet(viewsets.ModelViewSet):
