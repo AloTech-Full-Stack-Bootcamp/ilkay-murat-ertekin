518aa8209e3b5ad9df7ff5c95d07317e7b0bd008
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-18 18:04:23 +0300

    Add Jwt auth

--- week_8/patikagram/patikagram/settings.py
+++ week_8/patikagram/patikagram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_extensions',
+    'rest_framework_simplejwt',
 
     'main',
 ]
@@ -103,6 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
 
 
 # Internationalization
